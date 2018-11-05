aecb8ddc4640c64d0542615f510c0a1ac7981fc9
nijb <nijb@yonyou.com>
2018-11-05 17:16:51 +0800

    test

---
+++ src/statictest/StaticDemo.java
@@ -0,0 +1,16 @@
+package statictest;
+
+public class StaticDemo {
+
+	public static int wodesize = 1;
+	
+	public static void main(String[] args) {
+		StaticDemo demo1 = new StaticDemo();
+		//System.out.println(demo1.wodesize);
+		demo1.wodesize=2;
+		System.out.println(demo1.wodesize);
+		
+		StaticDemo demo2 = new StaticDemo();
+		System.out.println(demo2.wodesize);
+	}
+}
